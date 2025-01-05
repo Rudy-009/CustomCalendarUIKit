06ed3928ed486adc3f31b51e3c4dccbd1fad5690
Rudy-009 <rofxnaos@gmail.com>
2025-01-06 00:00:39 +0900

    Create UIKit Project

---
+++ CustomCalendarUIKit/CustomCalendarUIKit/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  CustomCalendarUIKit
+//
+//  Created by 이승준 on 1/5/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+
+
+}
